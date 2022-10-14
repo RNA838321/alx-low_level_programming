b5c0510381337ab20056c6588f51349c7d84c5de
RNA838321 <rodneyakong@gmail.com>
2022-10-14 05:14:00 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main-entry point for the program.
+ *
+ *Return:0 if no errors, return non zero if errors
+ */
+iniot main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of aet,\n");
+	return (0);
+}

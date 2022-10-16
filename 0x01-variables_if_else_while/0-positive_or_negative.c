b6d59802903420166731009e0401446b5848967b
RNA838321 <rodneyakong@gmail.com>
2022-10-15 17:39:31 -0700

    yes

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,10 +10,10 @@
 int main(void)
 {
 	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n>0)
+	
+	srand(0));
+	n = rand - RAND_MAX / 2;
+	if (n > 0)
 	{
 		printf("%d is positive\n", n);
 	}
@@ -23,8 +23,99 @@ int main(void)
 	}
 	else if (n < 0)
 	{
-		printf("%d is negative\n", n);
+		printf(%d is negative\n", n);
 	}
 	return (0);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 

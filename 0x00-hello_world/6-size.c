fb599d3328121e4fced06518c7e13b6f4dc362a5
RNA838321 <rodneyakong@gmail.com>
2022-10-16 16:20:56 -0700

    yes i can

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
 /**
- * main-entry point for the program.
+ *main -is the entry point for the program.
  *
- * Return:0 if no error, return non zero if errors
+ *Return:zero if no error, Non-zero value  if errors.
  */
 int main(void)
 {
 	printf("Size of a char: %ld byte(s)\n", sizeof(char));
-	printf(Size of an int: %d bytes(s)\n", sizeof(int));
+	printf("Size of an int: %ld byte(s)\n", sizeof(int));
 	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %lld byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %f byte(s)\n", sizeof(float));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %ld byte(s)\n", sizeof(float));
 	return (0);
 }
-
-	

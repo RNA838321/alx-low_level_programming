8eec3654917704cf57929ad5e9e0a11c36161af8
RNA838321 <rodneyakong@gmail.com>
2022-10-21 20:02:48 -0700

    fixed bug

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+#include <unistd.h>
 #include <stdio.h>
 int _isupper(int c);
 int _isdigit(int c);
@@ -10,7 +10,7 @@ void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
-_putchar
+int _putchar(char c)
 
 
 #endif

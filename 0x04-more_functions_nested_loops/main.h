7d76d197cf0c532120fc9c58c518e82c3883091f
RNA838321 <rodneyakong@gmail.com>
2022-10-22 14:36:05 -0700

    header file

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,7 +3,7 @@
 #include <unistd.h>
 #include <stdio.h>
 
-int _putchar(char c)
+int _putchar(char c) 
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -11,9 +11,8 @@ void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
-void print_square(int size);
 void print_diagonal(int n);
-
+void print_square(int size);
 
 
 #endif
